035f733ebd25c0907169f49d13b048ca8dd6cd94
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-02-09 09:47:33 +0100

    added methods

--- src/character/character.da.js
+++ src/character/character.da.js
@@ -4,22 +4,37 @@ const {CassandraClient} = require('../database/cassandra-client.database');
 
 module.exports = {
   getAllCharactersDB() {
-    // implement here ...
+    const query = 'SELECT * FROM workshop.characters';
+    return CassandraClient.execute(query)
+    .then(results => {
+      return results.rows.map(row => mapToCharacterDB(row['id'], row['name'], row['house'], row['allegiance']));
+    });
   },
 
   getCharacterById(id) {
-    // implement here ...
+    const params = [types.Uuid.fromString(id)];
+    const query = 'SELECT * FROM workshop.characters WHERE id=?';
+    return CassandraClient.execute(query, params)
+    .then(result => result.first())
+    .then(row => mapToCharacterDB(row['id'], row['name'], row['house'], row['allegiance']));
   },
 
   insertCharacter(characterToAdd) {
-    // implement here ...
+    const uuid = types.TimeUuid.now();
+    const params = [uuid, characterToAdd.name, characterToAdd.house, characterToAdd.allegiance];
+    const query = `INSERT INTO workshop.characters(id,name,house,allegiance) VALUES (?,?,?,?)`;
+    return CassandraClient.execute(query, params).then(() => uuid.toString());
   },
 
   updateCharacter(id, characterToUpdate) {
-    // implement here ...
+    const params = [characterToUpdate.name, characterToUpdate.house, characterToUpdate.allegiance, types.Uuid.fromString(id)]
+    const query = `UPDATE workshop.characters SET name=?, house=?, allegiance=? WHERE id=?`;
+    return CassandraClient.execute(query, params).then(resQuery => !!resQuery); 
   },
 
   deleteCharacter(characterIdToDelete) {
-    // implement here ...
+    const params = [characterIdToDelete];
+    const query = `DELETE FROM workshop.characters WHERE id=?`;
+    return CassandraClient.execute(query,params).then(resQuery => !!resQuery);
   }
 };
